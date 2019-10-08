3a84b19bc3be5ee6c09c2b1d55227a686ceb91f7
sarar <sara.henriksen95@hotmail.com>
2019-10-08 11:26:57 +0200

    dashboard er lavet, men har brug justeringer på nav og dashboard.

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,99 @@
-($(function() {
-    $('.chart').easypiechart({
-        scaleColor: "#ecf0f1",
-        lineWidth: 20,
-        lineCap: 'butt',
-        barColor: '#1abc9c',
-        trackColor:	"#ecf0f1",
-        size: 160,
-        animate: 500
+
+Chart.defaults.global.legend.position="bottom";
+
+    var  mychart =  document.getElementById("lineChart").getContext("2d");
+    var myLineChart = new Chart(lineCtx, {
+        type: 'line',
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fill: true,
+                    lineTension: 0.2,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(75,192,192,1)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 10,
+                    pointHoverBackgroundColor: "rgba(255,0,0,1)",
+                    pointHoverBorderColor: "rgba(255,0,0,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    spanGaps: false,
+                }
+            ]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    display: true
+                }]
+            }
+        }
     });
-}))
-;
+
+    var barCtx = document.getElementById("barChart");
+    var myBarChart = new Chart(barCtx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 2
+            }, {
+                label: '# of Votes2',
+                data: [25, 13, 17, 12, 24, 5],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
